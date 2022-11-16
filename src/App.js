bdeeb49f2a4217958d876a270022ce86ffbfb5b4
mooneesh <mooneesh@gmail.com>
2022-11-16 22:17:07 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
+
+  const expenses = [
+    {
+      id:'e1', 
+    title:'Toilet Paper', 
+    amount:10000, 
+    date: new Date(2020, 7, 14),
+  },
+    {
+      id:'e2', title:'LG TV', amount:20000, date: new Date(2022, 2, 12)  },
+    {
+      id:'e3', 
+    title:'Car Insurance', 
+    amount:25000, 
+    date: new Date(2022, 2, 28)
+  },
+    {
+      id:'e4',
+     title:'New Desk (Wooden)', 
+     amount:1000, 
+     date: new Date(2022, 2, 28)
+    },
+    
+  ];
+
   return (
     <div>
-      <h2>Let's Get Started</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
+      </div>
   );
 }
 
